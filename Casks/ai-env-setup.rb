f351a6dfde342d4b1d945f9df830715fe7c060a4
instasergio <instasergio@ya.ru>
2026-03-31 02:06:54 +0300

    Update ai-env-setup to 1.0.86

--- Casks/ai-env-setup.rb
+++ Casks/ai-env-setup.rb
@@ -1,6 +1,6 @@
 cask "ai-env-setup" do
-  version "1.0.85"
-  sha256 "e9396d824d01a7e1f6b3999960ab8b6864de3e82ae78fb7e2a17f588a7340f3d"
+  version "1.0.86"
+  sha256 "57e9394fbfa72102e493f1ba616a8c96012a6932ddc51a05beeb60a134606e61"
 
   url "https://github.com/instasergio/homebrew-tap/releases/download/build-#{version}-local-1/ai-env-setup-v#{version.major_minor}-build#{version.patch}.zip",
       verified: "github.com/instasergio/homebrew-tap/"
